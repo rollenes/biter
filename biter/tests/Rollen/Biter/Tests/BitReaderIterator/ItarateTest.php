7260b71895611d093e308aae53444e906b66f4d0
rollen <rafal.wartalski@gmail.com>
2014-06-17 22:56:34 +0200

    test fix

--- biter/tests/Rollen/Biter/Tests/BitReaderIterator/ItarateTest.php
+++ biter/tests/Rollen/Biter/Tests/BitReaderIterator/ItarateTest.php
@@ -2,7 +2,7 @@
 
 namespace Rollen\Biter\Tests\BitReaderIterator;
 
-class ItarateTest extends AbstractIterateTest
+class ItarateTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @test
@@ -12,9 +12,32 @@ class ItarateTest extends AbstractIterateTest
         $resource = $this->createResourceFromString('');
         
         $result = $this->getBitSequencesAsArray(new \Rollen\Biter\BitReaderIterator($resource));
-        
+
         $this->assertEmpty($result);
     }
+
     
     
+    
+    private function createResourceFromString($string)
+    {
+        $resource = fopen('php://memory', "w+");
+        
+        fwrite($resource, $string);
+        
+        rewind($resource);
+        
+        return $resource;
+    }
+    
+    private function getBitSequencesAsArray(\Rollen\Biter\BitReaderIterator $bitReaderIterator)
+    {
+        $result = [];
+        
+        foreach ($bitReaderIterator as $bitSequence) {
+            $result[] = $bitSequence;
+        }
+        
+        return $result;
+    }
 }
